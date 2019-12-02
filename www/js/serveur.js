21dd97b67fb038c390a99de5b5851d875f9950e0
Edouard DELATTRE <e_delattre62610@hotmail.fr>
2019-12-02 11:19:58 +0100

    Added some interraction between serveur and index.

--- www/js/serveur.js
+++ www/js/serveur.js
@@ -12,24 +12,44 @@ var server = http.createServer(function(req, res) {
 // Chargement de socket.io
 var io = require('socket.io').listen(server);
 
-// Quand un client se connecte, on le note dans la console
-io.sockets.on('connection', function (socket) {
-    console.log('Un client est connecté !');
-});
+let authorizedId ="Sélénium";
+let authorizedPassword ="Test";
+let authorizedId2 ="Poussière";
+let authorizedPassword2 ="Poussière";
 
-//Vers le client
-io.sockets.on('connection', function (socket) {
-    socket.emit('message', 'POUSSIERE');
+// Quand un client se connecte, on le note dans la console et on envoie un message
+io.sockets.on('connection', function (socket,pseudo) {
+    
+    console.log('Un client est connecté !');
+    //socket.emit('message', 'POUSSIERE');
+
+    //Envoie un message aux autres session
+    //socket.broadcast.emit('message', 'Une autre session en même temps');
 
     //Lorsque le client envoie un "message"
     socket.on('message', function(message){
-        console.log("Le message : " , message);
+        console.log(socket.pseudo, " Le message : " , message);
+    })
+
+    socket.on("nouveau_joueur", function(data){
+        if((data.identifiant==authorizedId && data.password==authorizedPassword) || (data.identifiant==authorizedId2 && data.password==authorizedPassword2))
+        {
+            socket.pseudo=data.identifiant;
+            socket.emit("nouveau_joueur", {identifiant: data.identifiant, password: data.password});
+            socket.broadcast.emit("nouvel_adversaire", {identifiant: data.identifiant, password: data.password});
+        }
     })
 
     //Faire des broadcast pour communiquer avec plusieurs clients
     //Variables de session pour save des trucs
 
+    socket.on("disconnect", function(){
+        socket.broadcast.emit("deconnexion", socket.pseudo);
+        console.log("client disconnected from server", socket.pseudo);
+    });
+
 });
+
 
 
 server.listen(8080);
