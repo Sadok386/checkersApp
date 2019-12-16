fb2ad08387429e6d152d2ebea7f28f1514271a93
Sadok Ben Fredj <benfreds@miashs-b1-6.u-ga.fr>
2019-12-16 11:46:02 +0100

    WIp js svg

---
+++ www/cherckers.js
@@ -0,0 +1,18 @@
+var svgNS = "http://www.w3.org/2000/svg";  
+
+function createCircle()
+{
+    var myCircle = document.createElementNS(svgNS,"circle"); //to create a circle. for rectangle use "rectangle"
+    myCircle.setAttributeNS(null,"id","mycircle");
+    myCircle.setAttributeNS(null,"cx",100);
+    myCircle.setAttributeNS(null,"cy",100);
+    myCircle.setAttributeNS(null,"r",50);
+    myCircle.setAttributeNS(null,"fill","black");
+    myCircle.setAttributeNS(null,"stroke","none");
+
+
+for ( property in object) {
+    
+  }
+    document.getElementById("mySVG").appendChild(myCircle);
+}     
