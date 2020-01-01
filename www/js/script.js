50cdf64d8f127cb3f9c8bba5c8933d295ea7eec5
SoleneAndre <49842961+SoleneAndre@users.noreply.github.com>
2020-01-01 20:48:11 +0100

    Ajout des fonctionnalites pour creer un user

---
+++ www/js/script.js
@@ -0,0 +1,106 @@
+var url = "http://localhost:8888";
+
+function creerUser() {
+     var username = document.getElementsByClassName("username");
+     var password = document.getElementsByClassName("password");
+
+     $.ajax({
+         url: url + "/creerCompte",
+         method: "post",
+         data: {
+             username: username[0].value,
+             password: password[0].value
+         }
+     }).success(function(response){
+         alert(response.message);
+     }).error(function(response) {
+         alert(response.message);
+     });
+}
+function ConnexionUser() {
+    var username = document.getElementsByClassName("username");
+    var password = document.getElementsByClassName("password");
+
+    $.ajax({
+        url: url + "/login",
+        method: "post",
+        data: {
+            username: username[0].value,
+            password: password[0].value
+        }
+        }).success(function(response){
+             window.location.assign("/home");
+         }).error(function(response) {
+             alert("Incorrect Pseudo ou mot de passe!");
+         });
+}
+function getUser() {
+    var username = document.getElementsByClassName("username");
+
+    $.ajax({
+        url: url + "/user",
+        method: "get"
+    }).success(function(response){
+        document.getElementsByClassName("username")[0].innerHTML = response.username;
+    }).error(function(response) {
+        alert("Impossible de charger les donneées. Essayer encore");
+    });
+    $.ajax({
+        url: url + "/items",
+        method: "get"
+    }).success(function(response) {
+
+        //Loop through each row and display them in the HTML
+        for(var a = 0; a < response.length; a++) {
+            var item = response[a];
+
+            //Display owner items data in the table
+            $("#itemtable").append("<tr>" +
+                "<td>" + item._id + "</td>" +
+                "<td>" + item.details + "</td>" +
+                "<td>" + item.post_time + "</td>" +
+                "<td>" + item.edit_time + "</td>" +
+                '<td><a href="">edit</a></td>' +
+                '<td><a href="">delete</a></td>' +
+                "<td>" + item.isPublic + "</td>" +
+            "</tr>");
+        }
+     }).error(function(response) {
+         alert("Cannot fetch data. Please try again");
+     });
+
+}
+
+function addItem() {
+    var details = document.getElementsByClassName("details")[0];
+    var isPublic = document.getElementsByClassName("isPublic")[0];
+
+    console.log(isPublic.checked);
+
+    $.ajax({
+        url: url + "/add",
+        method: "post",
+        data: {
+            details: details.value,
+            isPublic: isPublic.checked
+        }
+    }).success(function(response){
+        console.log(response);
+        alert("Item successfully added!");
+        //Mettre les donneées dans la table
+        ("#itemtable").append("<tr>" +
+            "<td>" + response.item._id + "</td>" +
+            "<td>" + response.item.details + "</td>" +
+            "<td>" + response.item.post_time + "</td>" +
+            "<td>" + response.item.edit_time + "</td>" +
+            '<td><a href="">edit</a></td>' +
+            '<td><a href="">delete</a></td>' +
+            "<td>" + response.item.isPublic + "</td>" +
+            "</tr>");
+
+    }).error(function(response) {
+        alert("Impossible d'ajouter l'objet, essayer encore");
+    });
+}
+
+
--- www/serveur/serveur.js
+++ www/serveur/serveur.js
@@ -1,54 +1,209 @@
-var io = require("socket.io");
-// Chargement de index après connexion
-var server = io.listen(8080);
 
-let authorizedId ="Sélénium";
-let authorizedPassword ="Test";
-let authorizedId2 ="Poussière";
-let authorizedPassword2 ="Poussière";
-console.log("Test");
-server.on("connection", function(socket){
-    console.log("Connection");
+var express = require ('express');
+var app = express();
+var port='8888'
+var mongoose = require('mongoose');
+var bodyParser = require ('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 
-    socket.on("nouveau_joueur", function(data){
-        console.log(data);
-        if((data.identifiant==authorizedId && data.password==authorizedPassword) || (data.identifiant==authorizedId2 && data.password==authorizedPassword2))
-        {
-            socket.pseudo=data.identifiant;
-            socket.emit("nouveau_joueur", {identifiant: data.identifiant, password: data.password});
-            socket.broadcast.emit("nouvel_adversaire", {identifiant: data.identifiant, password: data.password});
-        }
-    })
 
+app.use(bodyParser.urlencoded)(
+{
+extended: true;
+})
+}
+app.use('/js', express.static(__dirname + '/js'));
+
+/*Initialiser les sessions*/
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(cookieSession({
+    name: 'session',
+    keys: ['key1', 'key2']
+}));
+
+/*Connexion à la base de donnée - MongoDB*/
+/*Initialize Passport*/
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+var username = 'siteUserAdmin';
+var password = 'password';
+
+var dbHost = 'localhost';
+var dbPort = '27017';
+var database = 'admin';
+
+var url = 'mongodb://' + username + ':' + password + '@' + dbHost + ':' + dbPort + '/' + database;
+console.log('mongodb connection = ' + url);
+
+mongoose.connect(url, function(err) {
+    if(err) {
+        console.log('erreur de connexion : ', err);
+    } else {
+        console.log('connexion etablie');
+    }
+});
+/***********
+On declare les modeles ici
+***********/
+
+//User model
+var UserSchema = new mongoose.Schema({
+     _id: mongoose.Schema.ObjectId,
+     username: String,
+     password: String
+ });
+
+var User = mongoose.model('user', UserSchema);
+
+//Item model
+var ItemSchema = new mongoose.Schema({
+    owner: String,
+    details: String,
+    post_time: String,
+    edit_time: String,
+    isPublic: Boolean
 });
 
-/*
-// Quand un client se connecte, on le note dans la console et on envoie un message
-io.sockets.on('connection', function (socket,pseudo) {
-    
-    console.log('Un client est connecté !');
-    //socket.emit('message', 'POUSSIERE');
+var Item = mongoose.model('item', ItemSchema);
 
-    //Envoie un message aux autres session
-    //socket.broadcast.emit('message', 'Une autre session en même temps');
+/***********
+Et en bas c'est les routes
+***********/
 
-    //Lorsque le client envoie un "message"
-    socket.on('message', function(message){
-        console.log(socket.pseudo, " Le message : " , message);
-    })
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.findOne({ username: username }, function (err, user) {
+            if(user !== null) {
+                var isPasswordCorrect = bcrypt.compareSync(password, user.password);
+                if(isPasswordCorrect) {
+                    console.log("Username and password correct!");
+                    return done(null, user);
+                } else {
+                    console.log("Password incorrect!");
+                    return done(null, false);
+                }
+           } else {
+               console.log("Username does not exist!");
+               return done(null, false);
+           }
+       });
+    }
+));
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
 
-    
+passport.deserializeUser(function(user, done) {
+    done(err, user);
+});
+app.post('/creerCompte', function (req, res, next) {
+var bcrypt = require('bcrypt-nodejs');
+// on regarde si nos mots de passe sont chiffrés avant d'être enregistrés
+app.post('/creerCompte', function (req, res, next) {
+    var password = bcrypt.hashSync(req.body.password);
+    req.body.password = password;
+});
+}
+User.create(req.body, function(err, saved) {
+    if(err) {
+        console.log(err);
+        res.json({ message : err });
+    } else {
+        res.json({ message : "User crée avec succès!"});
+    }
+});
 
-    //Faire des broadcast pour communiquer avec plusieurs clients
-    //Variables de session pour save des trucs
-
-    socket.on("disconnect", function(){
-        socket.broadcast.emit("deconnexion", socket.pseudo);
-        console.log("client disconnected from server", socket.pseudo);
+app.get('/items', loggedIn, function (req, res, next) {
+    Item.find({ owner: req.user.username }, function(err, item) {
+        return res.json(item);
     });
-
 });
 
+function loggedIn(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect('/');
+    }
+}
+
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + " - " + hour + ":" + min + ":" + sec;
+
+}
+app.listen(port, '0.0.0.0', function(){
+console.log('le serveur est sur le port '+ port)}
+)
+app.get('/creerCompte', function (req, res, next) {
+    res.sendFile( __dirname + '/creerCompte.html');
+});
+
+app.get('/home', function (req, res, next) {
+    res.sendFile( __dirname + '/creerCompte.html');
+});
+
+app.get('/home', loggedIn, function (req, res, next) {
+     res.sendFile( __dirname + '/home.html');
+});
+
+app.post('/login', passport.authenticate('local'),
+    function(req, res) {
+        res.redirect('/home');
+});
+
+app.post('/creerCompte', function (req, res, next) {
+     console.log('username = ' + req.body.username);
+     console.log('password = ' + req.body.password);
+});
+
+app.get('/user', loggedIn, function (req, res, next) {
+    User.findById({ _id: req.user._id }, function(err, user) {
+        return res.json(user);
+    });
+});
+
+app.get('/logout', function (req, res, next) {
+    req.logout();
+    res.redirect('/');
+});
+
+function loggedIn(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect('/');
+    }
+}
 
 
-server.listen(8080); */
+app.get('/logout', function (req, res, next) {
+    req.logout();
+    res.redirect('/');
+});
+
